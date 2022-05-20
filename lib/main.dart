4821448164267a06fc3254ae6277468fb75034fd
nalynq9 <janisdinosaur@gmail.com>
2022-05-20 17:29:22 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key:key);
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
+      home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Flutter App"),
+          ),
+          body: Center(
+              child: Text("MyApp")
+          ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("ADD"),
+          onPressed: (){
+            print("Test");
+          },
+        ),
+      ),
+    );
+  }
+}
